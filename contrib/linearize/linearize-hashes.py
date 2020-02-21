95af189c72051f8a38c4199b6f278c6faf272fe3
Tentocat <109132670+Tentocat@users.noreply.github.com>
2020-02-21 15:58:03 +0000

    Done?

--- contrib/linearize/linearize-hashes.py
+++ contrib/linearize/linearize-hashes.py
@@ -85,4 +85,73 @@ def get_block_hashes(settings, max_blocks_per_call=10000):
 			return None
 
 		for x,resp_obj in enumerate(reply):
-			if rpc.response_is_erro
+			if rpc.response_is_error(resp_obj):
+				print('JSON-RPC: error at height', height+x, ': ', resp_obj['error'], file=sys.stderr)
+				sys.exit(1)
+			assert(resp_obj['id'] == x) # assume replies are in-sequence
+			if settings['rev_hash_bytes'] == 'true':
+				resp_obj['result'] = hex_switchEndian(resp_obj['result'])
+			print(resp_obj['result'])
+
+		height += num_blocks
+
+def get_rpc_cookie():
+	# Open the cookie file
+	with open(os.path.join(os.path.expanduser(settings['datadir']), '.cookie'), 'r') as f:
+		combined = f.readline()
+		combined_split = combined.split(":")
+		settings['rpcuser'] = combined_split[0]
+		settings['rpcpassword'] = combined_split[1]
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print("Usage: linearize-hashes.py CONFIG-FILE")
+		sys.exit(1)
+
+	f = open(sys.argv[1])
+	for line in f:
+		# skip comment lines
+		m = re.search('^\s*#', line)
+		if m:
+			continue
+
+		# parse key=value lines
+		m = re.search('^(\w+)\s*=\s*(\S.*)$', line)
+		if m is None:
+			continue
+		settings[m.group(1)] = m.group(2)
+	f.close()
+
+	if 'host' not in settings:
+		settings['host'] = '127.0.0.1'
+	if 'port' not in settings:
+		settings['port'] = 8332
+	if 'min_height' not in settings:
+		settings['min_height'] = 0
+	if 'max_height' not in settings:
+		settings['max_height'] = 313000
+	if 'rev_hash_bytes' not in settings:
+		settings['rev_hash_bytes'] = 'false'
+
+	use_userpass = True
+	use_datadir = False
+	if 'rpcuser' not in settings or 'rpcpassword' not in settings:
+		use_userpass = False
+	if 'datadir' in settings and not use_userpass:
+		use_datadir = True
+	if not use_userpass and not use_datadir:
+		print("Missing datadir or username and/or password in cfg file", file=sys.stderr)
+		sys.exit(1)
+
+	settings['port'] = int(settings['port'])
+	settings['min_height'] = int(settings['min_height'])
+	settings['max_height'] = int(settings['max_height'])
+
+	# Force hash byte format setting to be lowercase to make comparisons easier.
+	settings['rev_hash_bytes'] = settings['rev_hash_bytes'].lower()
+
+	# Get the rpc user and pass from the cookie if the datadir is set
+	if use_datadir:
+		get_rpc_cookie()
+
+	get_block_hashes(settings)
