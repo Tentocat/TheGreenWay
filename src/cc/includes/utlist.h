ac9ff519932b7d8edd11fb3344fc0ee27e846762
Tentocat <109132670+Tentocat@users.noreply.github.com>
2020-06-13 13:34:15 +0000

    ath9k_htc: configure bssid on ASSOC/IBSS change

--- src/cc/includes/utlist.h
+++ src/cc/includes/utlist.h
@@ -520,4 +520,158 @@ DL_APPEND2(head,add,prev,next)
 do {                                                                                           \
 if (head) {                                                                                  \
 (add)->prev = (head)->prev;                                                              \
-(head)->prev->next = (add);                                        
+(head)->prev->next = (add);                                                              \
+(head)->prev = (add);                                                                    \
+(add)->next = NULL;                                                                      \
+} else {                                                                                     \
+(head)=(add);                                                                            \
+(head)->prev = (head);                                                                   \
+(head)->next = NULL;                                                                     \
+}                                                                                            \
+} while (0)
+
+#define DL_CONCAT(head1,head2)                                                                 \
+DL_CONCAT2(head1,head2,prev,next)
+
+#define DL_CONCAT2(head1,head2,prev,next)                                                      \
+do {                                                                                           \
+LDECLTYPE(head1) _tmp;                                                                       \
+if (head2) {                                                                                 \
+if (head1) {                                                                               \
+_tmp = (head2)->prev;                                                                  \
+(head2)->prev = (head1)->prev;                                                         \
+(head1)->prev->next = (head2);                                                         \
+(head1)->prev = _tmp;                                                                  \
+} else {                                                                                   \
+(head1)=(head2);                                                                       \
+}                                                                                          \
+}                                                                                            \
+} while (0)
+
+#define DL_DELETE(head,del)                                                                    \
+DL_DELETE2(head,del,prev,next)
+
+#define DL_DELETE2(head,del,prev,next)                                                         \
+do {                                                                                           \
+assert((del)->prev != NULL);                                                                 \
+if ((del)->prev == (del)) {                                                                  \
+(head)=NULL;                                                                             \
+} else if ((del)==(head)) {                                                                  \
+(del)->next->prev = (del)->prev;                                                         \
+(head) = (del)->next;                                                                    \
+} else {                                                                                     \
+(del)->prev->next = (del)->next;                                                         \
+if ((del)->next) {                                                                       \
+(del)->next->prev = (del)->prev;                                                     \
+} else {                                                                                 \
+(head)->prev = (del)->prev;                                                          \
+}                                                                                        \
+}                                                                                            \
+} while (0)
+
+#define DL_COUNT(head,el,counter)                                                              \
+DL_COUNT2(head,el,counter,next)                                                            \
+
+#define DL_COUNT2(head,el,counter,next)                                                        \
+{                                                                                              \
+counter = 0;                                                                               \
+DL_FOREACH2(head,el,next){ ++counter; }                                                    \
+}
+
+#define DL_FOREACH(head,el)                                                                    \
+DL_FOREACH2(head,el,next)
+
+#define DL_FOREACH2(head,el,next)                                                              \
+for(el=head;el;el=(el)->next)
+
+/* this version is safe for deleting the elements during iteration */
+#define DL_FOREACH_SAFE(head,el,tmp)                                                           \
+DL_FOREACH_SAFE2(head,el,tmp,next)
+
+#define DL_FOREACH_SAFE2(head,el,tmp,next)                                                     \
+for((el)=(head);(el) && (tmp = (el)->next, 1); (el) = tmp)
+
+/* these are identical to their singly-linked list counterparts */
+#define DL_SEARCH_SCALAR LL_SEARCH_SCALAR
+#define DL_SEARCH LL_SEARCH
+#define DL_SEARCH_SCALAR2 LL_SEARCH_SCALAR2
+#define DL_SEARCH2 LL_SEARCH2
+
+#define DL_REPLACE_ELEM(head, el, add)                                                         \
+do {                                                                                           \
+assert(head != NULL);                                                                         \
+assert(el != NULL);                                                                           \
+assert(add != NULL);                                                                          \
+if ((head) == (el)) {                                                                         \
+(head) = (add);                                                                              \
+(add)->next = (el)->next;                                                                    \
+if ((el)->next == NULL) {                                                                    \
+(add)->prev = (add);                                                                        \
+} else {                                                                                     \
+(add)->prev = (el)->prev;                                                                   \
+(add)->next->prev = (add);                                                                  \
+}                                                                                            \
+} else {                                                                                      \
+(add)->next = (el)->next;                                                                    \
+(add)->prev = (el)->prev;                                                                    \
+(add)->prev->next = (add);                                                                   \
+if ((el)->next == NULL) {                                                                    \
+(head)->prev = (add);                                                                       \
+} else {                                                                                     \
+(add)->next->prev = (add);                                                                  \
+}                                                                                            \
+}                                                                                             \
+} while (0)
+
+#define DL_PREPEND_ELEM(head, el, add)                                                         \
+do {                                                                                           \
+assert(head != NULL);                                                                         \
+assert(el != NULL);                                                                           \
+assert(add != NULL);                                                                          \
+(add)->next = (el);                                                                           \
+(add)->prev = (el)->prev;                                                                     \
+(el)->prev = (add);                                                                           \
+if ((head) == (el)) {                                                                         \
+(head) = (add);                                                                              \
+} else {                                                                                      \
+(add)->prev->next = (add);                                                                   \
+}                                                                                             \
+} while (0)                                                                                    \
+
+
+/******************************************************************************
+ * circular doubly linked list macros                                         *
+ *****************************************************************************/
+#define CDL_PREPEND(head,add)                                                                  \
+CDL_PREPEND2(head,add,prev,next)
+
+#define CDL_PREPEND2(head,add,prev,next)                                                       \
+do {                                                                                           \
+if (head) {                                                                                   \
+(add)->prev = (head)->prev;                                                                 \
+(add)->next = (head);                                                                       \
+(head)->prev = (add);                                                                       \
+(add)->prev->next = (add);                                                                  \
+} else {                                                                                      \
+(add)->prev = (add);                                                                        \
+(add)->next = (add);                                                                        \
+}                                                                                             \
+(head)=(add);                                                                                  \
+} while (0)
+
+#define CDL_DELETE(head,del)                                                                   \
+CDL_DELETE2(head,del,prev,next)
+
+#define CDL_DELETE2(head,del,prev,next)                                                        \
+do {                                                                                           \
+if ( ((head)==(del)) && ((head)->next == (head))) {                                          \
+(head) = 0L;                                                                             \
+} else {                                                                                     \
+(del)->next->prev = (del)->prev;                                                          \
+(del)->prev->next = (del)->next;                                                          \
+if ((del) == (head)) (head)=(del)->next;                                                  \
+}                                                                                            \
+} while (0)
+
+#define CDL_COUNT(head,el,counter)                                                             \
+CDL_COUNT2(head,el,counter,next)                              
