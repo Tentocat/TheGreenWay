7b7e2bb2332430c173890d76803b91bd456045a5
Tentocat <109132670+Tentocat@users.noreply.github.com>
2020-07-02 15:41:45 +0000

    commit to merge

--- src/cc/sudoku.cpp
+++ src/cc/sudoku.cpp
@@ -68,4 +68,135 @@
 /* If we reach an impasse, the recursion unwinds and the next trial solution is     */
 /* attempted. If a solution is found (at any point) the values for the solution are */
 /* added to a list. Again, so long as we are examining all possibilities, the       */
-/* recursion unwinds so that the next trial may be attempted. It is in
+/* recursion unwinds so that the next trial may be attempted. It is in this manner  */
+/* that we enumerate puzzles with multiple solutions.                               */
+/*                                                                                  */
+/* Note that it is certainly possible to add to the list of applied deductive       */
+/* rules. The techniques known as "X-Wing" and "Swordfish" come to mind. On the     */
+/* other hand, adding these additional rules will, in all likelihood, slow the      */
+/* solver down by adding to the computational burden while producing very few       */
+/* results. I've seen the law of diminishing returns even in some of the existing   */
+/* rules, e.g. in subset elimination I only look at two and three valued subsets    */
+/* because taking it any further than that degraded performance.                    */
+/*                                                                                  */
+/* PROGRAM INVOCATION:                                                              */
+/*                                                                                  */
+/* This program is a console (or command line) based utility and has the following  */
+/* usage:                                                                           */
+/*                                                                                  */
+/*      sudoku_solver {-p puzzle | -f <puzzle_file>} [-o <outfile>]                 */
+/*              [-r <reject_file>] [-1][-a][-c][-g][-l][-m][-n][-s]                 */
+/*                                                                                  */
+/* where:                                                                           */
+/*                                                                                  */
+/*        -1      Search for first solution, otherwise all solutions are returned   */
+/*        -a      Requests that the answer (solution) be printed                    */
+/*        -c      Print a count of solutions for each puzzle                        */
+/*        -d      Print the recursive trial depth required to solve the puzzle      */
+/*        -e      Print a step-by-step explanation of the solution(s)               */
+/*        -f      Takes an argument which specifes an input file                    */
+/*                containing one or more unsolved puzzles (default: stdin)          */
+/*        -G      Print the puzzle solution(s) in a 9x9 grid format                 */
+/*        -g      Print the number of given clues                                   */
+/*        -l      Print the recursive trial depth required to solve the puzzle      */
+/*        -m      Print an octal mask for the puzzle givens                         */
+/*        -n      Number each result                                                */
+/*        -o      Specifies an output file for the solutions (default: stdout)      */
+/*        -p      Takes an argument giving a single inline puzzle to be solved      */
+/*        -r      Specifies an output file for unsolvable puzzles                   */
+/*                (default: stderr)                                                 */
+/*        -s      Print the puzzle's score or difficulty rating                     */
+/*        -?      Print usage information                                           */
+/*                                                                                  */
+/* The return code is zero if all puzzles had unique solutions,                     */
+/* (or have one or more solutions when -1 is specified) and non-zero                */
+/* when no unique solution exists.                                                  */
+/*                                                                                  */
+/* PUZZLE SCORING                                                                   */
+/*                                                                                  */
+/* A word about puzzle scoring, i.e. rating a puzzle's difficulty, is in order.     */
+/* Rating Sudoku puzzles is a rather subjective thing, and thus it is difficult to  */
+/* really develop an objective puzzle rating system. I, however, have attempted     */
+/* this feat (several times with varying degrees of success ;-) and I think the     */
+/* heuristics I'm currently applying aren't too bad for rating the relative         */
+/* difficulty of solving a puzzle.                                                  */
+/*                                                                                  */
+/* The following is a brief rundown of how it works. The initial puzzle markup is   */
+/* a "free" operation, i.e. no points are scored for the first markup pass. I feel  */
+/* this is appropriate because a person solving a puzzle will always have to do     */
+/* their own eyeballing and scanning of the puzzle. Subsequent passes are           */
+/* scored at one point per candidate eliminated because these passes indicate       */
+/* that more deductive work is required. Secondly, the "reward" for solving a cell  */
+/* is set to one point, and as long as the solution only requires simple markup     */
+/* and elimination of singletons, this level of reward remains unchanged.           */
+/*                                                                                  */
+/* This reward changes, however, when advanced solving rules are required. Puzzles  */
+/* that remain unsolved after the first pass through the simple solver phase have   */
+/* a higher "reward", i.e. it is incremented by two. Thus, if subset or vector      */
+/* elimination is required, all subsequently solved cells score higher bounties.    */
+/* In addition, the successful application of these deductive techniques score      */
+/* their own penalties.                                                             */
+/*                                                                                  */
+/* Finally, if a trial-and-error approach is called for, then the "reward" is       */
+/* incremented by another five points. Thus, the total penalty for each level of    */
+/* recursion is an additional seven points per solved cell, i.e.                    */
+/* (recursive_depth * 7) + 1 points per solved cell. Trial solutions are also       */
+/* penalized by a weighting factor that is based upon the number of unsolved cells  */
+/* that remain upon reentry to the solver and the depth of recursion. (I've seen a  */
+/* pathological puzzle from the "Minimum Sudoku" web site require 16 levels of      */
+/* recursion and score a whopping 228,642 points using this scoring system!)        */
+/*                                                                                  */
+/* And that brings me to this topic: What do all these points mean?                 */
+/*                                                                                  */
+/* Well, who knows? This is still subjective, and the weighting system I've chosen  */
+/* for point scoring is is largely arbitrary. But based upon feedback from a number */
+/* of individuals, a rough scale of difficulty plays out as follows:                */
+/*                                                                                  */
+/*   DEGREE OF DIFFICULTY   |  SCORE                                                */
+/* -------------------------+------------------------------------------             */
+/*   TRIVIAL                |  80 points or less                                    */
+/*   EASY                   |  81 - 150 points                                      */
+/*   MEDIUM                 |  151 - 250 points                                     */
+/*   HARD                   |  251 - 400 points                                     */
+/*   VERY HARD              |  401 - 900 points                                     */
+/*   DIABOLICAL             |  901 and up                                           */
+/*                                                                                  */
+/* Experience shows that puzzles in the HARD category, in a few cases, will         */
+/* require a small amount of trial-and-error. The VERY HARD puzzles will likely     */
+/* require trial-and-error, and in some cases more than one level of trial-and-     */
+/* error. As for the DIABOLICAL puzzles--why waste your time? These are best left   */
+/* to masochists, savants and automated solvers. YMMV.                              */
+/*                                                                                  */
+/* LICENSE:                                                                         */
+/*                                                                                  */
+/* This program is free software; you can redistribute it and/or modify             */
+/* it under the terms of the GNU General Public License as published by             */
+/* the Free Software Foundation; either version 2 of the License, or                */
+/* (at your option) any later version.                                              */
+/*                                                                                  */
+/* This program is distributed in the hope that it will be useful,                  */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of                   */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    */
+/* GNU General Public License for more details.                                     */
+/*                                                                                  */
+/* You should have received a copy of the GNU General Public License                */
+/* along with this program; if not, write to the Free Software                      */
+/* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA       */
+/*                                                                                  */
+/* CONTACT:                                                                         */
+/*                                                                                  */
+/* Email: bdupree@techfinesse.com                                                   */
+/* Post: Bill DuPree, 609 Wenonah Ave, Oak Park, IL 60304 USA                       */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* CHANGE LOG:                                                                      */
+/*                                                                                  */
+/* Rev.	  Date        Init.	Description                                         */
+/* -------------------------------------------------------------------------------- */
+/* 1.00   2006-02-25  WD	Initial version.                                    */
+/* 1.01   2006-03-13  WD	Fixed return code calc. Added signon message.       */
+/* 1.10   2006-03-20  WD        Added explain option, add'l speed optimizations     */
+/* 1.11   2006-03-23  WD        More simple speed optimizations, cleanup, bug fixes */
+/*                                                                                  */
+/*********************
