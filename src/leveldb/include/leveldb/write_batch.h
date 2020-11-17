d8028680b586acf33323a01a124c81fb33844029
Tentocat <109132670+Tentocat@users.noreply.github.com>
2020-11-17 18:47:36 +0000

    55

--- src/leveldb/include/leveldb/write_batch.h
+++ src/leveldb/include/leveldb/write_batch.h
@@ -33,4 +33,32 @@ class WriteBatch {
   WriteBatch();
   ~WriteBatch();
 
-  // Store the mapping "key->value" in the 
+  // Store the mapping "key->value" in the database.
+  void Put(const Slice& key, const Slice& value);
+
+  // If the database contains a mapping for "key", erase it.  Else do nothing.
+  void Delete(const Slice& key);
+
+  // Clear all updates buffered in this batch.
+  void Clear();
+
+  // Support for iterating over the contents of a batch.
+  class Handler {
+   public:
+    virtual ~Handler();
+    virtual void Put(const Slice& key, const Slice& value) = 0;
+    virtual void Delete(const Slice& key) = 0;
+  };
+  Status Iterate(Handler* handler) const;
+
+ private:
+  friend class WriteBatchInternal;
+
+  std::string rep_;  // See comment in write_batch.cc for the format of rep_
+
+  // Intentionally copyable
+};
+
+}  // namespace leveldb
+
+#endif  // STORAGE_LEVELDB_INCLUDE_WRITE_BATCH_H_
