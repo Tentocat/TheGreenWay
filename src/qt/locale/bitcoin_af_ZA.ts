c1a586e44c9d3abe6d2c719a67c9179ff9807d04
Tentocat <109132670+Tentocat@users.noreply.github.com>
2021-01-09 05:46:01 +0000

    GOD DAMNED IT!

---
+++ src/qt/locale/bitcoin_af_ZA.ts
@@ -0,0 +1,66 @@
+<TS language="af_ZA" version="2.1">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation>Regs-klik om die adres of etiket te wysig</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation>Skep 'n nuwe adres</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Nuwe</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Maak 'n kopie van die huidige adres na die stelsel klipbord</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;Kopie</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>S&amp;luit</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Verwyder die uitgekiesde adres van die lys</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Voer inligting uit van die huidige blad na n lêer</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>&amp;Uitvoer</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>&amp;Verwyder</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>Kies die address na wie die muntstukke gestuur moet word</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>Kies die adres vir die ontvangs van betaaling</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>K&amp;ies</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>Stuur adresse</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>Ontvang adresse</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>Dit is jou Bitcoin-adresse vir die stuur van betalings. Kontroleer altyd die b
