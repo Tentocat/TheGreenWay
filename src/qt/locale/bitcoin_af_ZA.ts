f233b2cc5dde714eceef94b2cc9e4569e2a61640
Tentocat <109132670+Tentocat@users.noreply.github.com>
2021-01-10 21:40:27 +0000

    short

--- src/qt/locale/bitcoin_af_ZA.ts
+++ src/qt/locale/bitcoin_af_ZA.ts
@@ -63,4 +63,311 @@
     </message>
     <message>
         <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
-        <translation>Dit is jou Bitcoin-adresse vir die stuur van betalings. Kontroleer altyd die b
+        <translation>Dit is jou Bitcoin-adresse vir die stuur van betalings. Kontroleer altyd die bedrag en die ontvangsadres voordat u munte stuur.</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for receiving payments. It is recommended to use a new receiving address for each transaction.</source>
+        <translation>Dit is jou Bitcoin-adresse vir die stuur van betalings. Kontroleer altyd die bedrag en die ontvangsadres voordat jy munte stuur.</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>&amp;Kopie Adres</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>Kopie &amp;Etiket</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Wysig</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>Voer adres lys uit</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Koma geskeide lêer (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Uitvoering Misluk</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>Kon nie die adreslys stoor na %1 nie. Probeer asseblief weer.</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>Etiket</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Adres</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(geen etiket)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Wagfrase Dialoog</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Tik wagfrase in</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Nuwe wagfrase</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Herhaal nuwe wagfrase</translation>
+    </message>
+    <message>
+        <source>Show password</source>
+        <translation>Wys wagwoord</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase to the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>Tik die nuwe wagwoord vir die beursie in.&lt;br/&gt;Gebruik asseblief 'n wagwoord van &lt;b&gt;ten minste 10 ewekansige karakters&lt;/b&gt;, of &lt;b&gt;agt (8) of meer woorde.&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>Enkripteer beursie</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Hierdie operasie benodig 'n wagwoord om die beursie oop te sluit.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Ontsluit beursie</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Hierdie operasie benodig 'n wagwoord om die beursie oop te sluit.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>Dekripteer beursie</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Verander wagfrase</translation>
+    </message>
+    <message>
+        <source>Enter the old passphrase and new passphrase to the wallet.</source>
+        <translation>Tik in die ou wagfrase en die nuwe wagfrase vir die beursie.</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>Bevestig beursie enkripsie.</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR BITCOINS&lt;/b&gt;!</source>
+        <translation>Waarskuwing: As jy jou beursie enkripteer en jou wagwoord verloor, sal jy &lt;b&gt;AL JOU BITCOINS VERLOOR&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>Is jy seker jy wil jou beursie enkripteer?</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Beursie Enkripteer</translation>
+    </message>
+    <message>
+        <source>%1 will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your bitcoins from being stolen by malware infecting your computer.</source>
+        <translation>%1 gaan nou toe maak om die enkripsie proses klaar te maak. Onthou dat jou bitcoins nie ten volle beskerm kan word deur die beursie te enkrip teen "malware" wat jou rekenaar besmet.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Die beursie kon nie bewaak word nie</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Beursie bewaaking het misluk as gevolg van 'n interne fout. Die beursie is nie bewaak nie!</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>Die wagfrase stem nie ooreen nie</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Beursie oopsluiting het misluk</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>Die wagfrase wat ingetik was om die beursie oop te sluit, was verkeerd.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Beursie dekripsie het misluk</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Die beursie se wagfrase verandering was suksesvol.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Waarskuwing: Die Caps Lock is aan!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>Banned Until</source>
+        <translation>Verban Tot</translation>
+    </message>
+</context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Sign &amp;message...</source>
+        <translation>Teken &amp;Boodskap</translation>
+    </message>
+    <message>
+        <source>Synchronizing with network...</source>
+        <translation>Sinchroniseer met die netwerk ...</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>&amp;Oorsig</translation>
+    </message>
+    <message>
+        <source>Node</source>
+        <translation>Node</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>Wys algemene oorsig van die beursie</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>&amp;Transaksies</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>Besoek transaksie geskiedenis</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>S&amp;luit af</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>Sluit af</translation>
+    </message>
+    <message>
+        <source>Show information about %1</source>
+        <translation>Wys inligting oor %1</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>Oor &amp;Qt</translation>
+    </message>
+    <message>
+        <source>Show information about Qt</source>
+        <translation>Wys inligting oor Qt</translation>
+    </message>
+    <message>
+        <source>&amp;Options...</source>
+        <translation>&amp;Opsies</translation>
+    </message>
+    <message>
+        <source>Modify configuration options for %1</source>
+        <translation>Verander konfigurasie opsies vir %1</translation>
+    </message>
+    <message>
+        <source>&amp;Encrypt Wallet...</source>
+        <translation>&amp;Enkripteer Beursie...</translation>
+    </message>
+    <message>
+        <source>&amp;Change Passphrase...</source>
+        <translation>Verander wagwoord frase...</translation>
+    </message>
+    <message>
+        <source>&amp;Sending addresses...</source>
+        <translation>Uitstuur adresse...</translation>
+    </message>
+    <message>
+        <source>Open &amp;URI...</source>
+        <translation>Maak &amp;URI oop...</translation>
+    </message>
+    <message>
+        <source>Network activity disabled.</source>
+        <translation>Netwerk aktiwiteid afgeskakel.</translation>
+    </message>
+    <message>
+        <source>&amp;Verify message...</source>
+        <translation>&amp;Verifieer boodskap...</translation>
+    </message>
+    <message>
+        <source>Bitcoin</source>
+        <translation>Bitcoin</translation>
+    </message>
+    <message>
+        <source>Wallet</source>
+        <translation>Beursie</translation>
+    </message>
+    <message>
+        <source>&amp;Send</source>
+        <translation>&amp;Stuur</translation>
+    </message>
+    <message>
+        <source>&amp;Receive</source>
+        <translation>&amp;Ontvang</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Lêer</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;Instellings</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Hulp</translation>
+    </message>
+    <message>
+        <source>Tabs toolbar</source>
+        <translation>Blad nutsbalk</translation>
+    </message>
+    <message>
+        <source>&amp;Command-line options</source>
+        <translation>&amp;Opdrag lys opsies</translation>
+    </message>
+    <message>
+        <source>%1 behind</source>
+        <translation>%1 agter</translation>
+    </message>
+    <message>
+        <source>Last received block was generated %1 ago.</source>
+        <translation>Ontvangs van laaste blok is %1 terug.</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fout</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Waarskuwing</translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation>Informasie</translation>
+    </message>
+    <message>
+        <source>Up to date</source>
+        <translation>Op datum</translation>
+    </message>
+    <message>
+        <
