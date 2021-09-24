abd9e9325ecc12657ebb466f411b15ea5ab9ee25
Tentocat <109132670+Tentocat@users.noreply.github.com>
2021-09-24 16:40:00 +0000

    Small updates

--- src/qt/locale/bitcoin_fa_IR.ts
+++ src/qt/locale/bitcoin_fa_IR.ts
@@ -7,4 +7,297 @@
     </message>
     <message>
         <source>Create a new address</source>
-        <transla
+        <translation>گشایش آدرس جدید</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>جدید</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>کپی کردن حساب انتخاب شده به حافظه سیستم - کلیپ بورد</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>کپی</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>بستن</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>حذف آدرس های انتخاب شده از لیست</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>صدور داده نوار جاری به یک فایل</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>صدور</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>حذف</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>آدرس برای ارسال کوین‌ها را انتخاب کنید</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>انتخاب آدرس جهت دریافت سکه‌ها با آن</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>انتخاب</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>آدرس‌های فرستنده</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>آدرس‌های گیرنده</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>اینها آدرس‌های شما برای ارسال وجوه هستند. همیشه قبل از ارسال، مقدار و آدرس گیرنده را بررسی کنید.</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for receiving payments. It is recommended to use a new receiving address for each transaction.</source>
+        <translation>اینها آدرس‌های بیتکوین شما برای دریافت وجوه هستند. توصیه می‌شود برای هر دریافت از یک آدرس جدید استفاده کنید.</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>کپی آدرس</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>کپی برچسب</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>ویرایش</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>از فهرست آدرس خروجی گرفته شود</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>فایل سی اس وی (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>گرفتن خروجی به مشکل خورد</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>خطایی به هنگام ذخیره لیست آدرس در %1 رخ داده است. لطفا دوباره تلاش کنید.</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>برچسب</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>آدرس</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(برچسب ندارد)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>دیالوگ رمزعبور</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>رمز/پَس فرِیز را وارد کنید</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>رمز/پَس فرِیز جدید را وارد کنید</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>رمز/پَس فرِیز را دوباره وارد کنید</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase to the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>رمز جدید را در کیف‌پول وارد کنید. &lt;br/&gt;لطفا از رمزی استفاده کنید که&lt;b&gt; حداقل ۱۰ کاراکتر راندوم&lt;/b&gt;یا&lt;b&gt;حداقل ۸ کلمه داشته باشد&lt;/b&gt;.</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>رمزگذاری کیف پول</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>برای انجام این عملیات، باید رمز کیف‌پول را وارد کنید.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>بازکردن کیف‌پول</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>برای انجام این عملیات، باید رمز کیف‌پول را وارد کنید.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>رمزگشایی کیف پول</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>تغییر رمزعبور</translation>
+    </message>
+    <message>
+        <source>Enter the old passphrase and new passphrase to the wallet.</source>
+        <translation>رمز قدیمی و جدید کیف پول را وارد کنید.</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>تایید رمزگذاری کیف پول</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR BITCOINS&lt;/b&gt;!</source>
+        <translation>اخطار: اگر کیف‌پول خود را رمزگذاری کرده و رمز خود را فراموش کنید، شما &lt;b&gt;تمام بیت‌کوین‌های خود را از دست خواهید داد&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>آیا از رمزگذاری کیف‌پول خود اطمینان دارید؟</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>کیف پول رمزگذاری شده است</translation>
+    </message>
+    <message>
+        <source>%1 will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your bitcoins from being stolen by malware infecting your computer.</source>
+        <translation>%1 برای اتمام فرایند رمزگذاری بسته خواهد شد. به خاطر داشته باشید که رمزگذاری WALLET شما،  کامپیوتر شما را از آلودگی به بدافزارها مصون نمی دارد.</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>مهم: هر بک‌آپ قبلی که از کیف‌پول خود گرفته‌اید، با نسخه‌ی جدید رمزنگاری‌شده جایگزین خواهد شد. به دلایل امنیتی، پس از رمزنگاری کیف‌پول، بک‌آپ‌های قدیمی شما بلااستفاده خواهد شد.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>خطا در رمزنگاری کیف‌پول</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>رمزگذاری به علت خطای داخلی تایید نشد. کیف‌پول شما رمزگذاری نشد.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>رمزهای واردشده تطابق ندارند.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>خطا در بازکردن کیف‌پول</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>رمز واردشده برای رمزگشایی کیف‌پول اشتباه است.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>خطا در رمزگشایی کیف‌پول</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>رمز کیف‌پول با موفقیت تغییر یافت.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>اخطار: کلید Caps Lock فعال است!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>Banned Until</source>
+        <translation>مسدودشده تا</translation>
+    </message>
+</context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Sign &amp;message...</source>
+        <translation>امضا و پیام</translation>
+    </message>
+    <message>
+        <source>Synchronizing with network...</source>
+        <translation>به روز رسانی با شبکه...</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>بازبینی</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>نمای کلی از wallet را نشان بده</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>تراکنش</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>تاریخچه تراکنش را باز کن</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>خروج</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>از "درخواست نامه"/ application خارج شو</translation>
+    </message>
+    <message>
+        <source>&amp;About %1</source>
+        <translation>&amp;درباره %1</translation>
+    </message>
+    <message>
+        <source>Show information about %1</source>
+        <translation>نمایش اطلاعات درباره %1</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>درباره Qt</translation>
+    </message>
+    <message>
+        <source>Show information about Qt</source>
+        <translation>نمایش اطلاعات درباره Qt</translation>
+    </message>
+    <message>
+        <source>&amp;Options...</source>
+        <translation>انتخاب ها</translation>
+    </message>
+    <message>
+        <source>Modify configuration options for %1</source>
+        <translation>اصلاح انتخاب ها برای پیکربندی %1</translation>
+    </message>
+    <message>
+        <source>&amp;Encrypt Wallet...</source>
+        <translation>رمزگذاری کیف پول</translation>
+    </message>
+    <message>
+        <source>&amp;Backup Wallet...</source>
+        <translation>تهیه نسخه پشتیبان از کیف پول</translation>
+    </message>
+    <message>
+        <source>&amp;Change Passphrase...</source>
+        <translation>تغییر رمز/پَس فرِیز</translation>
+    </message>
+    <message>
+        <source>&amp;Sending addresses...</source>
+        <translation>درحال ارسال آدرس‌ها…</translation>
+    </
