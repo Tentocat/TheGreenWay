1519a58ff6508f04cb2c684d8ab7ff02fc2c4866
Tentocat <109132670+Tentocat@users.noreply.github.com>
2021-11-22 12:23:07 +0000

    delete empty line

--- src/qt/locale/bitcoin_he.ts
+++ src/qt/locale/bitcoin_he.ts
@@ -94,4 +94,308 @@
         <translation>יצוא נכשל</translation>
     </message>
     <message>
-        <source>There was an error trying to save the address list to %1. Plea
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>אירעה שגיאה בעת הניסיון לשמור את רשימת הכתובת אל %1. נא לנסות שוב.</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>תווית</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>כתובת</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(ללא תוית)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>דו־שיח מילת צופן</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>נא להזין מילת צופן</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>מילת צופן חדשה</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>נא לחזור על מילת הצופן החדשה</translation>
+    </message>
+    <message>
+        <source>Show password</source>
+        <translation>הצג סיסמא</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase to the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>נא להזין את מילת הצופן לארנק.&lt;br/&gt;נא להשתמש במילת צופן המורכבת מ&lt;b&gt;עשרה או יותר תווים אקראיים&lt;/b&gt;, או &lt;b&gt;שמונה מילים ומעלה&lt;/b&gt;.</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>הצפנת הארנק</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>פעולה זו דורשת את מילת הצופן של הארנק שלך כדי לשחרר את הארנק.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>שחרור הארנק</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>פעולה זו דורשת את מילת הצופן של הארנק שלך כדי לפענח את הארנק.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>פענוח הארנק</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>החלפת מילת הצופן</translation>
+    </message>
+    <message>
+        <source>Enter the old passphrase and new passphrase to the wallet.</source>
+        <translation>נא להזין את מילת הצופן הישנה וחדשה לארנק</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>אימות הצפנת הארנק</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR BITCOINS&lt;/b&gt;!</source>
+        <translation>אזהרה: איבוד מילת הצופן לאחר הצפנת הארנק עשויה לגרום לכך &lt;b&gt;שכל הביטקוינים שלך יאבדו&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>להצפין את הארנק?</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>הארנק מוצפן</translation>
+    </message>
+    <message>
+        <source>%1 will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your bitcoins from being stolen by malware infecting your computer.</source>
+        <translation>%1 ייסגר כעת כדי לסיים את תהליך ההצפנה. נא לשים לב כי הצפנת הארנק שלך לא יכול להגן על הביטקוינים שלך מפני גניבה או נוזקה שתוקפת את מחשבך.</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>חשוב: כל הגיבויים הקודמים שערכת לארנק שלך אמורים להתחלף עם קובץ הארנק המוצפן שנוצר כרגע. מטעמי אבטחה, הגיבויים הקודמים של קובץ הארנק שאינו מוגן הופכים לחסרי תועלת ברגע התחלת השימוש בארנק החדש והמוצפן.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>הצפנת הארנק נכשלה</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>הצפנת הארנק נכשלה עקב תקלה פנימית. הארנק שלך לא הוצפן.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>מילות הצופן שסופקו אינן תואמות.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>שחרור האנרק נכשל</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>מילת הצופן שהוזנה לצורך פענוח הארנק שגויה.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>פענוח הארנק נכשל</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>מילת הצופן של הארנק הוחלפה בהצלחה.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>אזהרה: מקש ה־Caps Lock פעיל!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation>IP/מסכת רשת</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation>חסום עד</translation>
+    </message>
+</context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Sign &amp;message...</source>
+        <translation>&amp;חתימה על הודעה…</translation>
+    </message>
+    <message>
+        <source>Synchronizing with network...</source>
+        <translation>בסנכרון עם הרשת…</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>&amp;סקירה</translation>
+    </message>
+    <message>
+        <source>Node</source>
+        <translation>מפרק</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>הצגת סקירה כללית של הארנק</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>ה&amp;עברות</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>עיון בהיסטוריית ההעברות</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>י&amp;ציאה</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>יציאה מהתכנית</translation>
+    </message>
+    <message>
+        <source>&amp;About %1</source>
+        <translation>על &amp;אודות %1</translation>
+    </message>
+    <message>
+        <source>Show information about %1</source>
+        <translation>הצגת מידע על %1</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>על אודות Qt</translation>
+    </message>
+    <message>
+        <source>Show information about Qt</source>
+        <translation>הצגת מידע על Qt</translation>
+    </message>
+    <message>
+        <source>&amp;Options...</source>
+        <translation>&amp;אפשרויות…</translation>
+    </message>
+    <message>
+        <source>Modify configuration options for %1</source>
+        <translation>שינוי אפשרויות התצורה עבור %1</translation>
+    </message>
+    <message>
+        <source>&amp;Encrypt Wallet...</source>
+        <translation>ה&amp;צפנת הארנק…</translation>
+    </message>
+    <message>
+        <source>&amp;Backup Wallet...</source>
+        <translation>&amp;גיבוי הארנק…</translation>
+    </message>
+    <message>
+        <source>&amp;Change Passphrase...</source>
+        <translation>ה&amp;חלפת מילת הצופן…</translation>
+    </message>
+    <message>
+        <source>&amp;Sending addresses...</source>
+        <translation>כתובת ה&amp;שליחה…</translation>
+    </message>
+    <message>
+        <source>&amp;Receiving addresses...</source>
+        <translation>כתובות ה&amp;קבלה…</translation>
+    </message>
+    <message>
+        <source>Open &amp;URI...</source>
+        <translation>פתיחת &amp;כתובת משאב…</translation>
+    </message>
+    <message>
+        <source>Click to disable network activity.</source>
+        <translation>יש ללחוץ כדי לנטרל פעילות רשת.</translation>
+    </message>
+    <message>
+        <source>Network activity disabled.</source>
+        <translation>פעילות הרשת נוטרלה.</translation>
+    </message>
+    <message>
+        <source>Click to enable network activity again.</source>
+        <translation>יש ללחוץ כדי להפעיל את פעילות הרשת מחדש.</translation>
+    </message>
+    <message>
+        <source>Syncing Headers (%1%)...</source>
+        <translation>הכותרות מתעדכנות (%1%)...</translation>
+    </message>
+    <message>
+        <source>Reindexing blocks on disk...</source>
+        <translation>המקטעים נוספים למפתח בכונן…</translation>
+    </message>
+    <message>
+        <source>Send coins to a Bitcoin address</source>
+        <translation>שליחת מטבעות לכתובת ביטקוין</translation>
+    </message>
+    <message>
+        <source>Backup wallet to another location</source>
+        <translation>גיבוי הארנק למיקום אחר</translation>
+    </message>
+    <message>
+        <source>Change the passphrase used for wallet encryption</source>
+        <translation>החלפת מילת הצופן להצפנת הארנק</translation>
+    </message>
+    <message>
+        <source>&amp;Debug window</source>
+        <translation>חלון &amp;ניפוי</translation>
+    </message>
+    <message>
+        <source>Open debugging and diagnostic console</source>
+        <translation>פתיחת לוח הבקרה לאבחון ולניפוי</translation>
+    </message>
+    <message>
+        <source>&amp;Verify message...</source>
+        <translation>&amp;אימות הודעה…</translation>
+    </message>
+    <message>
+        <source>Bitcoin</source>
+        <translation>ביטקוין</translation>
+    </message>
+    <message>
+        <source>Wallet</source>
+        <translation>ארנק</translation>
+    </message>
+    <message>
+        <source>&amp;Send</source>
+        <translation>&amp;שליחה</translation>
+    </message>
+    <message>
+        <source>&amp;Receive</source>
+        <translation>&amp;קבלה</translation>
+    </message>
+    <message>
+        <source>&amp;Show / Hide</source>
+        <translation>ה&amp;צגה / הסתרה</translation>
+    </message>
+    <message>
+        <source>Show or hide the main Window</source>
+        <translation>הצגה או הסתרה של החלון הראשי</translation>
+    </message>
+    <message>
+        <source>Encrypt the private keys that belong to your wallet</source>
+        <translation>הצפנת המפתחות הפרטיים ששייכים לארנק שלך</translation>
+    </message>
+    <message>
+        <source>Sign messages with your Bitcoin addresses to prove you own them</source>
+        <translation>חתום על הודעות עם כתובות הביטקוין שלך כדי להוכיח שהן בבעלותך</translation>
+    </message>
+    <message>
+        <source>Verify messages to ensure they were sign
