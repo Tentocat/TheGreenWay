e56c5216e8db5803ff4990c63f77b73151f52158
Tentocat <109132670+Tentocat@users.noreply.github.com>
2021-12-02 22:47:40 +0000

    Add some debug messages

---
+++ src/qt/locale/bitcoin_is.ts
@@ -0,0 +1,238 @@
+<TS language="is" version="2.1">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation>Smelltu á hægri músatakka til að breyta færslugildi eða merkingu</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation>Búa til nýtt færslugildi</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Nýtt</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Afrita valið færslugildi í klemmuspjald</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;Afrita</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>&amp;Loka</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Eyða völdu færslugildi úr listanum</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Flytja gögn í flipanum í skrá</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>&amp;Flytja út</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>&amp;Eyða</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>Veldu færslugildi sem greiða skal til</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>Veldu færslugildi sem á að taka við mynt</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>&amp;Veldu</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>Færslugildi sem senda frá sér</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>Færslugildi sem þiggja til sín</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>Þetta eru Bitcoin færslugildin sem senda greiðslur. Skoðið ævinlega vel upphæðina og færslugildin sem þiggja greiðslur áður en mynt er send.</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for receiving payments. It is recommended to use a new receiving address for each transaction.</source>
+        <translation>Þetta eru Bitcoin færslugildin sem þiggja greiðslur. Mælt er með að nota aldrei sama færslugildið til að þiggja fleiri en eina greiðslu.</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>&amp;Afrita færslugildi</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>Afrita og &amp;Merkja</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Breyta</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>Flytja út færslulista</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Gildi aðskilin með kommu (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Útflutningur tókst ekki</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>Ekki tókst að vista færslugildalistann á %1. Reyndu aftur.</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>Merking</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Færslugildi</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(engin merking)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Lykilsetning</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Skráðu lykilsetningu</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Ný lykilsetning</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Endurtaktu nýja lykilsetningu</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase to the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>Skráðu nýju lykilsetninguna í veskið. &lt;br/&gt;Vinsamlegast notaðu lykilsetningu með &lt;b&gt;tíu eða fleiri slembibókstöfum&lt;/b&gt;, eða &lt;b&gt;átta eða fleiri orðum&lt;/b&gt;.</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>Dulkóða veski</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Þessi aðgerð þarf að fá lykilsetninguna þína til að opna veskið.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Opna veskið</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Þessi aðgerð þarf lykilsetninguna þína til að dulráða veskið.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>Dulráða veskið</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Breyta lykilsetningu</translation>
+    </message>
+    <message>
+        <source>Enter the old passphrase and new passphrase to the wallet.</source>
+        <translation>Skráðu gömlu lykilsetninguna og þá nýju í veskið.</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>Staðfesta dulkóðun veskis</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR BITCOINS&lt;/b&gt;!</source>
+        <translation>Viðvörun: Ef þú dulkóðar veskið og týnir lykilsetningunn þá munt þú &lt;b&gt;TAPA ALLRI ÞINNI BITCOIN MYNT&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>Ertu viss um að þú viljir dulkóða veskið þitt?</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Veski dulkóðað</translation>
+    </message>
+    <message>
+        <source>%1 will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your bitcoins from being stolen by malware infecting your computer.</source>
+        <translation>%1 lokast núna til að dulkóðun klárist. Mundu að dulkóðun veskis kemur ekki að fullu í veg fyrir að mynt verði stolið úr tölvunni þinni með aðstoð smitforrita. </translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>MIKILVÆGT: Nýja dulkóðaða veskisskráin þarf að koma í staðinn fyrir öll fyrri afrit sem þú hefur gert af upprunalegu veskisskránni. Af öryggisástæðum munu öll fyrri afrit af ódulkóðaða veskinu verða óvirk um leið og þú byrjar að nota nýja, dulkóðaða veskið.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Dulkóðun veskis mistókst</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Dulkóðun veskis mistóks vegna innri villu. Veskið þitt var ekki dulkóðað.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>Lykilsetningarnar eru ekki þær sömu.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Ekki tókst að opna veskið</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>Lykilsetningin sem notuð var til að dulráða veskið var ekki rétt.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Ekki tókst að dulráða veski</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Það tókst að breyta lykilsetningu veskis.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Viðvörun: Kveikt er á HÁSTÖFUM!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation>IP/Netgríma</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation>Bannað til</translation>
+    </message>
+</context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Sign &amp;message...</source>
+        <translation>Undirrita &amp;ski
