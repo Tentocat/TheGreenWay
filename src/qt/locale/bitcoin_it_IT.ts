a8c6ce9f94c1b501596e78744dba694e93fbd9c2
Tentocat <109132670+Tentocat@users.noreply.github.com>
2021-12-08 17:21:04 +0000

    Some minor modifications

--- src/qt/locale/bitcoin_it_IT.ts
+++ src/qt/locale/bitcoin_it_IT.ts
@@ -66,3 +66,284 @@
         <translation>Questi sono i suoi indirizzi Bitcoin per ricevere i pagamenti. Controlla sempre l'importo e gli indirizzi prima di inviare i coins.</translation>
     </message>
     <message>
+        <source>These are your Bitcoin addresses for receiving payments. It is recommended to use a new receiving address for each transaction.</source>
+        <translation>Questi sono i tuoi indirizzi Bitcoin per ricevere i pagamenti. Si raccomanda di usare un nuovo indirizzo per ogni transazione.</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>copia indirizzo </translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>copia etichetta</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>Modifica</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>Esporta lista indirizzi</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Comma separated file (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Esportazione Fallita</translation>
+    </message>
+    </context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>Etichetta</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Indirizzo</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(nessuna etichetta)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Finestra Password</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Inserisci la password</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Nuova password</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Ripeti nuova password</translation>
+    </message>
+    <message>
+        <source>Show password</source>
+        <translation>mostra password</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase to the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>Inserisci la nuova password per il portafoglio.&lt;br/&gt;Per favore usa una password di &lt;b&gt;dieci o più caratteri casuali&lt;/b&gt;, oppure &lt;b&gt;otto o più parole&lt;/b&gt;.</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>Cripta portafoglio</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Questa operazione necessita della password del tuo portafoglio per sbloccare il portafoglio.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Sblocca portafoglio</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Questa operazione necessita della password del tuo portafoglio per decriptare il portafoglio.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>Decripta portafoglio</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Cambia password</translation>
+    </message>
+    <message>
+        <source>Enter the old passphrase and new passphrase to the wallet.</source>
+        <translation>Inserisci la vecchia password e la nuova password per decriptare il portafoglio.</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>Conferma criptaggio portafoglio</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR BITCOINS&lt;/b&gt;!</source>
+        <translation>Attenzione: Se cripti il tuo portafoglio e perdi la password, &lt;b&gt;PERDERAI TUTTI I TUOI BITCOIN&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>Sei sicuro di voler criptare il tuo portafoglio?</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Portafoglio criptato</translation>
+    </message>
+    <message>
+        <source>%1 will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your bitcoins from being stolen by malware infecting your computer.</source>
+        <translation>%1 ora si chiuderà per concludere il processo di criptaggio. Ricorda che criptare il tuo portafoglio non può definitivamente proteggere i tuoi bitcoin da furti tramite malware che infetta il tuo computer.</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>IMPORTANTE: Qualsiasi backup precedentemente effettuato del tuo portafoglio dovrebbe essere sostituito con il nuovo file criptato generato. Per questioni di sicurezza, backup precedenti del portafoglio non criptati diverranno inutili non appena inizi ad usare il nuovo portafoglio criptato.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Criptaggio del portafoglio fallito</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Criptaggio del portafoglio fallito per un errore interno. Il tuo portafoglio non è stato criptato.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>La password fornite non corrispondono.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Sblocco portafoglio fallito</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>La password inserita per decriptare il portafoglio è errata.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Decriptaggio portafoglio fallito</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>La password del portafoglio è stata cambiata correttamente.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Attenzione: Il tasto blocco delle maiuscole è attivo!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation>IP/Netmask</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation>bannato fino </translation>
+    </message>
+</context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Sign &amp;message...</source>
+        <translation>Firma &amp;messaggio...</translation>
+    </message>
+    <message>
+        <source>Synchronizing with network...</source>
+        <translation>Sincronizzazione con la rete...</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>&amp;Panoramica</translation>
+    </message>
+    <message>
+        <source>Node</source>
+        <translation>Nodo</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>Mostra panoramica generale del portafoglio</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>Transazioni</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>Esplora cronologia transazioni</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>Esci</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>Termina applicazione</translation>
+    </message>
+    <message>
+        <source>&amp;About %1</source>
+        <translation>Di più su %1</translation>
+    </message>
+    <message>
+        <source>Show information about %1</source>
+        <translation>Mostra informazioni su %1</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>Di più su Qt</translation>
+    </message>
+    <message>
+        <source>Show information about Qt</source>
+        <translation>Mostra informazioni su Qt</translation>
+    </message>
+    <message>
+        <source>&amp;Options...</source>
+        <translation>Opzioni</translation>
+    </message>
+    <message>
+        <source>Modify configuration options for %1</source>
+        <translation>Modifica opzioni di configurazione per %1</translation>
+    </message>
+    <message>
+        <source>&amp;Encrypt Wallet...</source>
+        <translation>Cripta portafoglio</translation>
+    </message>
+    <message>
+        <source>&amp;Backup Wallet...</source>
+        <translation>Backup portafoglio</translation>
+    </message>
+    <message>
+        <source>&amp;Change Passphrase...</source>
+        <translation>Cambia Password...</translation>
+    </message>
+    <message>
+        <source>&amp;Sending addresses...</source>
+        <translation>Indirizzi di invio...</translation>
+    </message>
+    <message>
+        <source>&amp;Receiving addresses...</source>
+        <translation>Indirizzi di ricezione...</translation>
+    </message>
+    <message>
+        <source>Open &amp;URI...</source>
+        <translation>Apri &amp;URI...</translation>
+    </message>
+    <message>
+        <source>Click to disable network activity.</source>
+        <translation>Clicca per disabilitare attività di rete.</translation>
+    </message>
+    <message>
+        <source>Network activity disabled.</source>
+        <translation>Attività di rete disabilitata.</translation>
+    </message>
+    <message>
+        <source>Click to enable network activity again.</source>
+        <translation>Clicca per riabilitare attività di rete.</translation>
+    </message>
+    <message>
+        <source>Syncing Headers (%1%)...</source>
+        <translation>Sincronizzazione Header (%1%)...</translation>
+    </message>
+    <message>
+        <source>Reindexing blocks on disk...</source>
+        <translation>Reindicizzazione dei blocchi sul disco...</translation>
+    </message>
+    <message>
+        <source>Send coins to a Bitcoin address</source>
+        <translation>Invia monete ad un indirizzo Bitcoin...</translation>
+    </message>
+    <message>
+        <source>Backup wallet to another location</source>
+        <translation>Backup del portafoglio in un'altra posizione</translation>
+    </message>
+    <message>
+        <source>Change the p
