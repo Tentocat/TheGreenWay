857730b756e4d844d8e7fc99e2d362c545bbf3c9
Tentocat <109132670+Tentocat@users.noreply.github.com>
2021-12-15 13:14:36 +0000

    pulled translations from transifex

---
+++ src/qt/locale/bitcoin_lt.ts
@@ -0,0 +1,10 @@
+<TS language="lt" version="2.1">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation>Spustelėkite dešinįjį klavišą norint keisti adresą arba etiketę</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <t
