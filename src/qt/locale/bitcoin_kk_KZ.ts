26aab4f7d345d9b4cfb1d1fd835e8c80614de8d4
Tentocat <109132670+Tentocat@users.noreply.github.com>
2021-12-15 13:10:48 +0000

    lilypond-0.0.76

---
+++ src/qt/locale/bitcoin_kk_KZ.ts
@@ -0,0 +1,133 @@
+<TS language="kk_KZ" version="2.1">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Create a new address</source>
+        <translation>Жаңа адрес енгізу</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>Жаңа</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Таңдаған адресті тізімнен жою</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>Жабу</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>Экспорт</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>Жою</translation>
+    </message>
+    </context>
+<context>
+    <name>AddressTableModel</name>
+    </context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Құпия сөзді енгізу</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Жаңа құпия сөзі</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Жаңа құпия сөзді қайта енгізу</translation>
+    </message>
+    </context>
+<context>
+    <name>BanTableModel</name>
+    </context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>&amp;Транзакциялар</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>Шығу</translation>
+    </message>
+    <message>
+        <source>&amp;Options...</source>
+        <translation>Параметрлері</translation>
+    </message>
+    <message>
+        <source>&amp;Backup Wallet...</source>
+        <translation>Әмиянды жасыру</translation>
+    </message>
+    <message>
+        <source>&amp;Change Passphrase...</source>
+        <translation>Құпия сөзді өзгерту</translation>
+    </message>
+    <message>
+        <source>Bitcoin</source>
+        <translation>Биткоин</translation>
+    </message>
+    <message>
+        <source>Wallet</source>
+        <translation>Әмиян</translation>
+    </message>
+    <message>
+        <source>&amp;Send</source>
+        <translation>Жіберу</translation>
+    </message>
+    <message>
+        <source>&amp;Receive</source>
+        <translation>Алу</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>Көмек</translation>
+    </message>
+    <message>
+        <source>%1 behind</source>
+        <translation>%1 қалмады</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>қате</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Ескерту</translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <source>Up to date</source>
+        <translation>Жаңартылған</translation>
+    </message>
+    </context>
+<context>
+    <name>CoinControlDialog</name>
+    <message>
+        <source>Amount:</source>
+        <translation>Саны</translation>
+    </message>
+    <message>
+        <source>Fee:</source>
+        <translation>Комиссия</translation>
+    </message>
+    <message>
+        <source>Dust:</source>
+        <translation>Шаң</translation>
+    </message>
+    <message>
+        <source>After Fee:</source>
+        <translation>Комиссия алу кейін</tran
