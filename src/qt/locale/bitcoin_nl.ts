a92c1d310ecba99a3d8e6f1961fb14d8e848306d
Tentocat <109132670+Tentocat@users.noreply.github.com>
2022-01-10 18:37:39 +0000

    davinci: dm365evm_keys driver

---
+++ src/qt/locale/bitcoin_nl.ts
@@ -0,0 +1,41 @@
+<TS language="nl" version="2.1">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation>Rechtermuisklik om het adres of label te wijzigen</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation>Maak een nieuw adres aan</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Nieuw</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Kopieer het geselecteerde adres naar het klembord</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;Kopieer</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>S&amp;luiten</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Verwijder het geselecteerde adres van de lijst</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Exporteer de data in de huidige tab naar een bestand</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>&amp;Exporteer</translation>
+    </message>
+    <message>
+        <source>&amp;
