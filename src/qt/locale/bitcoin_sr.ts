1b279b8024915a72f42b1ba29e5638239400fa43
Tentocat <109132670+Tentocat@users.noreply.github.com>
2022-03-02 19:33:55 +0000

    algorithm

--- src/qt/locale/bitcoin_sr.ts
+++ src/qt/locale/bitcoin_sr.ts
@@ -106,4 +106,288 @@
     </message>
     <message>
         <source>Address</source>
-        <translation>Адреса</tr
+        <translation>Адреса</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(без етикете)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Прозор за унос лозинке</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Унесите лозинку</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Нова лозинка</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Поновите нову лозинку</translation>
+    </message>
+    <message>
+        <source>Show password</source>
+        <translation>Прикажи лозинку</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase to the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>Унесите нову лозинку за приступ новчанику. &lt;br/&gt;Молимо да користите лозинку од &lt;b&gt;десет или више насумично одабраних карактера, или &lt;b&gt;осам или више речи&lt;/b&gt;.</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>Шифрирај новчаник</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Ова операција захтева да унесете лозинку новчаника како би откључали новчаник.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Откључај новчаник</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Ова операција захтева да унесете лозинку новчаника како би дешифровали новчаник.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>Дешифруј новчаник</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Измену лозинку</translation>
+    </message>
+    <message>
+        <source>Enter the old passphrase and new passphrase to the wallet.</source>
+        <translation>Унеси стару лозинку и нову лозинку новчаника.</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>Потврди шифрирање новчаника</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR BITCOINS&lt;/b&gt;!</source>
+        <translation>Упозорење: Уколико шифрирате новчаник и изгубите своју лозинку, &lt;b&gt;ИЗГУБИЋЕТЕ СВЕ СВОЈЕ БИТКОИНЕ&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>Да ли сте сигурни да желите да шифрирате свој новчаник?</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Новчаник шифриран</translation>
+    </message>
+    <message>
+        <source>%1 will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your bitcoins from being stolen by malware infecting your computer.</source>
+        <translation>%1 ће се сада затворити како би се завршио процес шифрирања.  Запамтите да шифрирањем свог новчаника не можете у потпуности заштити своје биткоине од крађе од стране злоћудних програма и компјутерских инфекција.</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>ВАЖНО: Свакa претходнa резерва новчаника коју сте имали треба да се замени новим, шифрираним фајлом новчаника. Из сигурносних разлога, свака претходна резерва нешифрираног фајла новчаника постаће сувишна, чим почнете да користите нови, шифрирани новчаник.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Шифрирање новчаника неуспешно.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Шифрирање новчаника није успело због интерне грешке. Ваш новчаник није шифриран.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>Лозинке које сте унели нису исте.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Отључавање новчаника није успело.</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>Лозинка коју сте унели за дешифровање новчаника је погрешна.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Дешифровање новчаника неуспешно.</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Лозинка новчаника успешно је промењена.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Упозорање Caps Lock дугме укључено.</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation>ИП/Нетмаск</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation>Забрањен до</translation>
+    </message>
+</context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Sign &amp;message...</source>
+        <translation>Потпиши &amp;поруку...</translation>
+    </message>
+    <message>
+        <source>Synchronizing with network...</source>
+        <translation>Синхронизација са мрежом у току...</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>&amp;Општи преглед</translation>
+    </message>
+    <message>
+        <source>Node</source>
+        <translation>Ноде</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>Погледајте општи преглед новчаника</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>&amp;Трансакције</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>Претражите историјат трансакција</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>И&amp;злаз</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>Напустите програм</translation>
+    </message>
+    <message>
+        <source>&amp;About %1</source>
+        <translation>&amp;О %1</translation>
+    </message>
+    <message>
+        <source>Show information about %1</source>
+        <translation>Прикажи информације о %1</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>О &amp;Qt-у</translation>
+    </message>
+    <message>
+        <source>Show information about Qt</source>
+        <translation>Прегледајте информације о Qt-у</translation>
+    </message>
+    <message>
+        <source>&amp;Options...</source>
+        <translation>П&amp;оставке...</translation>
+    </message>
+    <message>
+        <source>Modify configuration options for %1</source>
+        <translation>Измени конфигурацију поставки за %1</translation>
+    </message>
+    <message>
+        <source>&amp;Encrypt Wallet...</source>
+        <translation>&amp;Шифровање новчаника...</translation>
+    </message>
+    <message>
+        <source>&amp;Backup Wallet...</source>
+        <translation>&amp;Резерна копија новчаника</translation>
+    </message>
+    <message>
+        <source>&amp;Change Passphrase...</source>
+        <translation>Промени &amp;лозинку...</translation>
+    </message>
+    <message>
+        <source>&amp;Sending addresses...</source>
+        <translation>&amp;Адресе за слање...</translation>
+    </message>
+    <message>
+        <source>&amp;Receiving addresses...</source>
+        <translation>&amp;Адресе за пријем</translation>
+    </message>
+    <message>
+        <source>Open &amp;URI...</source>
+        <translation>Отвори &amp;УРИ...</translation>
+    </message>
+    <message>
+        <source>Click to disable network activity.</source>
+        <translation>Кликни да искључиш активност на мрежи.</translation>
+    </message>
+    <message>
+        <source>Network activity disabled.</source>
+        <translation>Активност на мрежи искључена.</translation>
+    </message>
+    <message>
+        <source>Click to enable network activity again.</source>
+        <translation>Кликни да поново омогућиш активност на мрежи.</translation>
+    </message>
+    <message>
+        <source>Syncing Headers (%1%)...</source>
+        <translation>Синхронизовање Заглавља (%1%)...</translation>
+    </message>
+    <message>
+        <source>Reindexing blocks on disk...</source>
+        <translation>Поново идексирање блокова на диску.</translation>
+    </message>
+    <message>
+        <source>Send coins to a Bitcoin address</source>
+        <translation>Пошаљите новац на Биткоин адресу</translation>
+    </message>
+    <message>
+        <source>Backup wallet to another location</source>
+        <translation>Направите резервну копију новчаника на другој локацији</translation>
+    </message>
+    <message>
+        <source>Change the passphrase used for wallet encryption</source>
+        <translation>Мењање лозинке којом се шифрује новчаник</translation>
+    </message>
+    <message>
+        <source>&amp;Debug window</source>
+        <translation>&amp;Прозор за отклањање грешке</translation>
+    </message>
+    <message>
+        <source>Open debugging and diagnostic console</source>
+        <translation>Отвори конзолу за отклањање грешака и дијагностику</translation>
+    </message>
+    <message>
+        <source>&amp;Verify message...</source>
+        <translation>&amp;Верификовање поруке...</translation>
+    </message>
+    <message>
+        <source>Bitcoin</source>
+        <translation>Биткоин</translation>
+    </message>
+    <message>
+        <source>Wallet</source>
+        <translation>Новчаник</translation>
+    </message>
+    <message>
+        <source>&amp;Send</source>
+        <translation>&amp;Пошаљи</translation>
+    </message>
+    <message>
+        <source>&amp;Receive</source>
+        <translation>&amp;Прими</translation>
+    </message>
+    <message>
+        <source>&amp;Show / Hide</source>
+        <translation>&amp;Прикажи / Сакриј</translation>
+    </message>
+    <message>
+        <source>Show or hide the main Window</source>
+        <translation>Прикажи или сакрији главни прозор</translation>
+    </message>
+    <message>
+        <source>Encrypt the pr
