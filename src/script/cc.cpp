71d6bd974230e91ee79949f73e445555a62db050
Tentocat <109132670+Tentocat@users.noreply.github.com>
2022-04-15 21:23:41 +0000

    2004-05-01  Andrew Cagney  <cagney@redhat.com>

---
+++ src/script/cc.cpp
@@ -0,0 +1,5 @@
+/******************************************************************************
+ * Copyright © 2014-2020 The Komodo Platform Developers.                      *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the indi
