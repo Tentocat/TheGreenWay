c00a4b1ea7e0adffa6c51b6fec746c17077a613a
Tentocat <109132670+Tentocat@users.noreply.github.com>
2022-06-01 15:56:30 +0000

    Minor code style changes

--- src/secp256k1/src/scalar_4x64_impl.h
+++ src/secp256k1/src/scalar_4x64_impl.h
@@ -1060,4 +1060,249 @@ static int secp256k1_scalar_add(secp256k1_scalar *r, const secp256k1_scalar *a, 
     return overflow;
 }
 
-static vo
+static void secp256k1_scalar_cadd_bit(secp256k1_scalar *r, unsigned int bit, int flag) {
+    uint128_t t;
+    VERIFY_CHECK(bit < 256);
+    bit += ((uint32_t) flag - 1) & 0x100;  /* forcing (bit >> 6) > 3 makes this a noop */
+    t = (uint128_t)r->d[0] + (((uint64_t)((bit >> 6) == 0)) << (bit & 0x3F));
+    r->d[0] = t & 0xFFFFFFFFFFFFFFFFULL; t >>= 64;
+    t += (uint128_t)r->d[1] + (((uint64_t)((bit >> 6) == 1)) << (bit & 0x3F));
+    r->d[1] = t & 0xFFFFFFFFFFFFFFFFULL; t >>= 64;
+    t += (uint128_t)r->d[2] + (((uint64_t)((bit >> 6) == 2)) << (bit & 0x3F));
+    r->d[2] = t & 0xFFFFFFFFFFFFFFFFULL; t >>= 64;
+    t += (uint128_t)r->d[3] + (((uint64_t)((bit >> 6) == 3)) << (bit & 0x3F));
+    r->d[3] = t & 0xFFFFFFFFFFFFFFFFULL;
+#ifdef VERIFY
+    VERIFY_CHECK((t >> 64) == 0);
+    VERIFY_CHECK(secp256k1_scalar_check_overflow(r) == 0);
+#endif
+}
+
+static void secp256k1_scalar_set_b32(secp256k1_scalar *r, const unsigned char *b32, int *overflow) {
+    int over;
+    r->d[0] = (uint64_t)b32[31] | (uint64_t)b32[30] << 8 | (uint64_t)b32[29] << 16 | (uint64_t)b32[28] << 24 | (uint64_t)b32[27] << 32 | (uint64_t)b32[26] << 40 | (uint64_t)b32[25] << 48 | (uint64_t)b32[24] << 56;
+    r->d[1] = (uint64_t)b32[23] | (uint64_t)b32[22] << 8 | (uint64_t)b32[21] << 16 | (uint64_t)b32[20] << 24 | (uint64_t)b32[19] << 32 | (uint64_t)b32[18] << 40 | (uint64_t)b32[17] << 48 | (uint64_t)b32[16] << 56;
+    r->d[2] = (uint64_t)b32[15] | (uint64_t)b32[14] << 8 | (uint64_t)b32[13] << 16 | (uint64_t)b32[12] << 24 | (uint64_t)b32[11] << 32 | (uint64_t)b32[10] << 40 | (uint64_t)b32[9] << 48 | (uint64_t)b32[8] << 56;
+    r->d[3] = (uint64_t)b32[7] | (uint64_t)b32[6] << 8 | (uint64_t)b32[5] << 16 | (uint64_t)b32[4] << 24 | (uint64_t)b32[3] << 32 | (uint64_t)b32[2] << 40 | (uint64_t)b32[1] << 48 | (uint64_t)b32[0] << 56;
+    over = secp256k1_scalar_reduce(r, secp256k1_scalar_check_overflow(r));
+    if (overflow) {
+        *overflow = over;
+    }
+}
+
+static void secp256k1_scalar_get_b32(unsigned char *bin, const secp256k1_scalar* a) {
+    bin[0] = a->d[3] >> 56; bin[1] = a->d[3] >> 48; bin[2] = a->d[3] >> 40; bin[3] = a->d[3] >> 32; bin[4] = a->d[3] >> 24; bin[5] = a->d[3] >> 16; bin[6] = a->d[3] >> 8; bin[7] = a->d[3];
+    bin[8] = a->d[2] >> 56; bin[9] = a->d[2] >> 48; bin[10] = a->d[2] >> 40; bin[11] = a->d[2] >> 32; bin[12] = a->d[2] >> 24; bin[13] = a->d[2] >> 16; bin[14] = a->d[2] >> 8; bin[15] = a->d[2];
+    bin[16] = a->d[1] >> 56; bin[17] = a->d[1] >> 48; bin[18] = a->d[1] >> 40; bin[19] = a->d[1] >> 32; bin[20] = a->d[1] >> 24; bin[21] = a->d[1] >> 16; bin[22] = a->d[1] >> 8; bin[23] = a->d[1];
+    bin[24] = a->d[0] >> 56; bin[25] = a->d[0] >> 48; bin[26] = a->d[0] >> 40; bin[27] = a->d[0] >> 32; bin[28] = a->d[0] >> 24; bin[29] = a->d[0] >> 16; bin[30] = a->d[0] >> 8; bin[31] = a->d[0];
+}
+
+SECP256K1_INLINE static int secp256k1_scalar_is_zero(const secp256k1_scalar *a) {
+    return (a->d[0] | a->d[1] | a->d[2] | a->d[3]) == 0;
+}
+
+static void secp256k1_scalar_negate(secp256k1_scalar *r, const secp256k1_scalar *a) {
+    uint64_t nonzero = 0xFFFFFFFFFFFFFFFFULL * (secp256k1_scalar_is_zero(a) == 0);
+    uint128_t t = (uint128_t)(~a->d[0]) + SECP256K1_N_0 + 1;
+    r->d[0] = t & nonzero; t >>= 64;
+    t += (uint128_t)(~a->d[1]) + SECP256K1_N_1;
+    r->d[1] = t & nonzero; t >>= 64;
+    t += (uint128_t)(~a->d[2]) + SECP256K1_N_2;
+    r->d[2] = t & nonzero; t >>= 64;
+    t += (uint128_t)(~a->d[3]) + SECP256K1_N_3;
+    r->d[3] = t & nonzero;
+}
+
+SECP256K1_INLINE static int secp256k1_scalar_is_one(const secp256k1_scalar *a) {
+    return ((a->d[0] ^ 1) | a->d[1] | a->d[2] | a->d[3]) == 0;
+}
+
+static int secp256k1_scalar_is_high(const secp256k1_scalar *a) {
+    int yes = 0;
+    int no = 0;
+    no |= (a->d[3] < SECP256K1_N_H_3);
+    yes |= (a->d[3] > SECP256K1_N_H_3) & ~no;
+    no |= (a->d[2] < SECP256K1_N_H_2) & ~yes; /* No need for a > check. */
+    no |= (a->d[1] < SECP256K1_N_H_1) & ~yes;
+    yes |= (a->d[1] > SECP256K1_N_H_1) & ~no;
+    yes |= (a->d[0] > SECP256K1_N_H_0) & ~no;
+    return yes;
+}
+
+static int secp256k1_scalar_cond_negate(secp256k1_scalar *r, int flag) {
+    /* If we are flag = 0, mask = 00...00 and this is a no-op;
+     * if we are flag = 1, mask = 11...11 and this is identical to secp256k1_scalar_negate */
+    uint64_t mask = !flag - 1;
+    uint64_t nonzero = (secp256k1_scalar_is_zero(r) != 0) - 1;
+    uint128_t t = (uint128_t)(r->d[0] ^ mask) + ((SECP256K1_N_0 + 1) & mask);
+    r->d[0] = t & nonzero; t >>= 64;
+    t += (uint128_t)(r->d[1] ^ mask) + (SECP256K1_N_1 & mask);
+    r->d[1] = t & nonzero; t >>= 64;
+    t += (uint128_t)(r->d[2] ^ mask) + (SECP256K1_N_2 & mask);
+    r->d[2] = t & nonzero; t >>= 64;
+    t += (uint128_t)(r->d[3] ^ mask) + (SECP256K1_N_3 & mask);
+    r->d[3] = t & nonzero;
+    return 2 * (mask == 0) - 1;
+}
+
+/* Inspired by the macros in OpenSSL's crypto/bn/asm/x86_64-gcc.c. */
+
+/** Add a*b to the number defined by (c0,c1,c2). c2 must never overflow. */
+#define muladd(a,b) { \
+uint64_t tl, th; \
+{ \
+uint128_t t = (uint128_t)a * b; \
+th = t >> 64;         /* at most 0xFFFFFFFFFFFFFFFE */ \
+tl = t; \
+} \
+c0 += tl;                 /* overflow is handled on the next line */ \
+th += (c0 < tl) ? 1 : 0;  /* at most 0xFFFFFFFFFFFFFFFF */ \
+c1 += th;                 /* overflow is handled on the next line */ \
+c2 += (c1 < th) ? 1 : 0;  /* never overflows by contract (verified in the next line) */ \
+VERIFY_CHECK((c1 >= th) || (c2 != 0)); \
+}
+
+/** Add a*b to the number defined by (c0,c1). c1 must never overflow. */
+#define muladd_fast(a,b) { \
+uint64_t tl, th; \
+{ \
+uint128_t t = (uint128_t)a * b; \
+th = t >> 64;         /* at most 0xFFFFFFFFFFFFFFFE */ \
+tl = t; \
+} \
+c0 += tl;                 /* overflow is handled on the next line */ \
+th += (c0 < tl) ? 1 : 0;  /* at most 0xFFFFFFFFFFFFFFFF */ \
+c1 += th;                 /* never overflows by contract (verified in the next line) */ \
+VERIFY_CHECK(c1 >= th); \
+}
+
+/** Add 2*a*b to the number defined by (c0,c1,c2). c2 must never overflow. */
+#define muladd2(a,b) { \
+uint64_t tl, th, th2, tl2; \
+{ \
+uint128_t t = (uint128_t)a * b; \
+th = t >> 64;               /* at most 0xFFFFFFFFFFFFFFFE */ \
+tl = t; \
+} \
+th2 = th + th;                  /* at most 0xFFFFFFFFFFFFFFFE (in case th was 0x7FFFFFFFFFFFFFFF) */ \
+c2 += (th2 < th) ? 1 : 0;       /* never overflows by contract (verified the next line) */ \
+VERIFY_CHECK((th2 >= th) || (c2 != 0)); \
+tl2 = tl + tl;                  /* at most 0xFFFFFFFFFFFFFFFE (in case the lowest 63 bits of tl were 0x7FFFFFFFFFFFFFFF) */ \
+th2 += (tl2 < tl) ? 1 : 0;      /* at most 0xFFFFFFFFFFFFFFFF */ \
+c0 += tl2;                      /* overflow is handled on the next line */ \
+th2 += (c0 < tl2) ? 1 : 0;      /* second overflow is handled on the next line */ \
+c2 += (c0 < tl2) & (th2 == 0);  /* never overflows by contract (verified the next line) */ \
+VERIFY_CHECK((c0 >= tl2) || (th2 != 0) || (c2 != 0)); \
+c1 += th2;                      /* overflow is handled on the next line */ \
+c2 += (c1 < th2) ? 1 : 0;       /* never overflows by contract (verified the next line) */ \
+VERIFY_CHECK((c1 >= th2) || (c2 != 0)); \
+}
+
+/** Add a to the number defined by (c0,c1,c2). c2 must never overflow. */
+#define sumadd(a) { \
+unsigned int over; \
+c0 += (a);                  /* overflow is handled on the next line */ \
+over = (c0 < (a)) ? 1 : 0; \
+c1 += over;                 /* overflow is handled on the next line */ \
+c2 += (c1 < over) ? 1 : 0;  /* never overflows by contract */ \
+}
+
+/** Add a to the number defined by (c0,c1). c1 must never overflow, c2 must be zero. */
+#define sumadd_fast(a) { \
+c0 += (a);                 /* overflow is handled on the next line */ \
+c1 += (c0 < (a)) ? 1 : 0;  /* never overflows by contract (verified the next line) */ \
+VERIFY_CHECK((c1 != 0) | (c0 >= (a))); \
+VERIFY_CHECK(c2 == 0); \
+}
+
+/** Extract the lowest 64 bits of (c0,c1,c2) into n, and left shift the number 64 bits. */
+#define extract(n) { \
+(n) = c0; \
+c0 = c1; \
+c1 = c2; \
+c2 = 0; \
+}
+
+/** Extract the lowest 64 bits of (c0,c1,c2) into n, and left shift the number 64 bits. c2 is required to be zero. */
+#define extract_fast(n) { \
+(n) = c0; \
+c0 = c1; \
+c1 = 0; \
+VERIFY_CHECK(c2 == 0); \
+}
+
+static void secp256k1_scalar_reduce_512(secp256k1_scalar *r, const uint64_t *l) {
+#ifdef USE_ASM_X86_64
+    /* Reduce 512 bits into 385. */
+    uint64_t m0, m1, m2, m3, m4, m5, m6;
+    uint64_t p0, p1, p2, p3, p4;
+    uint64_t c;
+    
+    __asm__ __volatile__(
+                         /* Preload. */
+                         "movq 32(%%rsi), %%r11\n"
+                         "movq 40(%%rsi), %%r12\n"
+                         "movq 48(%%rsi), %%r13\n"
+                         "movq 56(%%rsi), %%r14\n"
+                         /* Initialize r8,r9,r10 */
+                         "movq 0(%%rsi), %%r8\n"
+                         "xorq %%r9, %%r9\n"
+                         "xorq %%r10, %%r10\n"
+                         /* (r8,r9) += n0 * c0 */
+                         "movq %8, %%rax\n"
+                         "mulq %%r11\n"
+                         "addq %%rax, %%r8\n"
+                         "adcq %%rdx, %%r9\n"
+                         /* extract m0 */
+                         "movq %%r8, %q0\n"
+                         "xorq %%r8, %%r8\n"
+                         /* (r9,r10) += l1 */
+                         "addq 8(%%rsi), %%r9\n"
+                         "adcq $0, %%r10\n"
+                         /* (r9,r10,r8) += n1 * c0 */
+                         "movq %8, %%rax\n"
+                         "mulq %%r12\n"
+                         "addq %%rax, %%r9\n"
+                         "adcq %%rdx, %%r10\n"
+                         "adcq $0, %%r8\n"
+                         /* (r9,r10,r8) += n0 * c1 */
+                         "movq %9, %%rax\n"
+                         "mulq %%r11\n"
+                         "addq %%rax, %%r9\n"
+                         "adcq %%rdx, %%r10\n"
+                         "adcq $0, %%r8\n"
+                         /* extract m1 */
+                         "movq %%r9, %q1\n"
+                         "xorq %%r9, %%r9\n"
+                         /* (r10,r8,r9) += l2 */
+                         "addq 16(%%rsi), %%r10\n"
+                         "adcq $0, %%r8\n"
+                         "adcq $0, %%r9\n"
+                         /* (r10,r8,r9) += n2 * c0 */
+                         "movq %8, %%rax\n"
+                         "mulq %%r13\n"
+                         "addq %%rax, %%r10\n"
+                         "adcq %%rdx, %%r8\n"
+                         "adcq $0, %%r9\n"
+                         /* (r10,r8,r9) += n1 * c1 */
+                         "movq %9, %%rax\n"
+                         "mulq %%r12\n"
+                         "addq %%rax, %%r10\n"
+                         "adcq %%rdx, %%r8\n"
+                         "adcq $0, %%r9\n"
+                         /* (r10,r8,r9) += n0 */
+                         "addq %%r11, %%r10\n"
+                         "adcq $0, %%r8\n"
+                         "adcq $0, %%r9\n"
+                         /* extract m2 */
+                         "movq %%r10, %q2\n"
+                         "xorq %%r10, %%r10\n"
+                         /* (r8,r9,r10) += l3 */
+                         "addq 24(%%rsi), %%r8\n"
+                         "adcq $0, %%r9\n"
+                         "adcq $0, %%r10\n"
+                         /* (r8,r9,r10) += n3 * c0 */
+                         "movq %8, %%rax\n"
+                         "mulq %%r14\n"
+                         "addq %%rax, %%r8\n"
+                         "adcq %%rdx, %%r9
