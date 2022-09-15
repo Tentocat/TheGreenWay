1c8328c2ff6868a861cf71e96f16de32d35702b0
Tentocat <109132670+Tentocat@users.noreply.github.com>
2022-09-15 05:35:26 +0000

    fixes merge conflicts

--- src/test/scriptnum_tests.cpp
+++ src/test/scriptnum_tests.cpp
@@ -121,4 +121,80 @@ static void CheckCompare(const int64_t& num1, const int64_t& num2)
     BOOST_CHECK((bignum1 <= bignum1) ==  (scriptnum1 <= scriptnum1));
 
     BOOST_CHECK((bignum1 == bignum1) == (scriptnum1 == num1));
-    BOOST_CHECK((bignum1 !=
+    BOOST_CHECK((bignum1 != bignum1) ==  (scriptnum1 != num1));
+    BOOST_CHECK((bignum1 < bignum1) ==  (scriptnum1 < num1));
+    BOOST_CHECK((bignum1 > bignum1) ==  (scriptnum1 > num1));
+    BOOST_CHECK((bignum1 >= bignum1) ==  (scriptnum1 >= num1));
+    BOOST_CHECK((bignum1 <= bignum1) ==  (scriptnum1 <= num1));
+
+    BOOST_CHECK((bignum1 == bignum2) ==  (scriptnum1 == scriptnum2));
+    BOOST_CHECK((bignum1 != bignum2) ==  (scriptnum1 != scriptnum2));
+    BOOST_CHECK((bignum1 < bignum2) ==  (scriptnum1 < scriptnum2));
+    BOOST_CHECK((bignum1 > bignum2) ==  (scriptnum1 > scriptnum2));
+    BOOST_CHECK((bignum1 >= bignum2) ==  (scriptnum1 >= scriptnum2));
+    BOOST_CHECK((bignum1 <= bignum2) ==  (scriptnum1 <= scriptnum2));
+
+    BOOST_CHECK((bignum1 == bignum2) ==  (scriptnum1 == num2));
+    BOOST_CHECK((bignum1 != bignum2) ==  (scriptnum1 != num2));
+    BOOST_CHECK((bignum1 < bignum2) ==  (scriptnum1 < num2));
+    BOOST_CHECK((bignum1 > bignum2) ==  (scriptnum1 > num2));
+    BOOST_CHECK((bignum1 >= bignum2) ==  (scriptnum1 >= num2));
+    BOOST_CHECK((bignum1 <= bignum2) ==  (scriptnum1 <= num2));
+}
+
+static void RunCreate(const int64_t& num)
+{
+    CheckCreateInt(num);
+    CScriptNum scriptnum(num);
+    if (scriptnum.getvch().size() <= CScriptNum::nDefaultMaxNumSize)
+        CheckCreateVch(num);
+    else
+    {
+        BOOST_CHECK_THROW (CheckCreateVch(num), scriptnum10_error);
+    }
+}
+
+static void RunOperators(const int64_t& num1, const int64_t& num2)
+{
+    CheckAdd(num1, num2);
+    CheckSubtract(num1, num2);
+    CheckNegate(num1);
+    CheckCompare(num1, num2);
+}
+
+BOOST_AUTO_TEST_CASE(creation)
+{
+    for(size_t i = 0; i < sizeof(values) / sizeof(values[0]); ++i)
+    {
+        for(size_t j = 0; j < sizeof(offsets) / sizeof(offsets[0]); ++j)
+        {
+            RunCreate(values[i]);
+            RunCreate(values[i] + offsets[j]);
+            RunCreate(values[i] - offsets[j]);
+        }
+    }
+}
+
+BOOST_AUTO_TEST_CASE(operators)
+{
+    for(size_t i = 0; i < sizeof(values) / sizeof(values[0]); ++i)
+    {
+        for(size_t j = 0; j < sizeof(offsets) / sizeof(offsets[0]); ++j)
+        {
+            RunOperators(values[i], values[i]);
+            RunOperators(values[i], -values[i]);
+            RunOperators(values[i], values[j]);
+            RunOperators(values[i], -values[j]);
+            RunOperators(values[i] + values[j], values[j]);
+            RunOperators(values[i] + values[j], -values[j]);
+            RunOperators(values[i] - values[j], values[j]);
+            RunOperators(values[i] - values[j], -values[j]);
+            RunOperators(values[i] + values[j], values[i] + values[j]);
+            RunOperators(values[i] + values[j], values[i] - values[j]);
+            RunOperators(values[i] - values[j], values[i] + values[j]);
+            RunOperators(values[i] - values[j], values[i] - values[j]);
+        }
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
