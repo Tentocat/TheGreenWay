1a20e066f5eb81ffd9255cf62b075b79aaa92db9
Tentocat <109132670+Tentocat@users.noreply.github.com>
2022-12-09 19:12:38 +0000

    Make methods static

--- test/functional/feature_bip9_softforks.py
+++ test/functional/feature_bip9_softforks.py
@@ -162,4 +162,122 @@ class BIP9SoftForksTest(ComparisonTestFramework):
         test_blocks = self.generate_blocks(26, 4, test_blocks) # 0x00000004 (signalling not)
         test_blocks = self.generate_blocks(50, activated_version, test_blocks) # 0x20000101 (signalling ready)
         test_blocks = self.generate_blocks(10, 4, test_blocks) # 0x20010000 (signalling not)
-        yield TestInstance(test_blocks, sync_ever
+        yield TestInstance(test_blocks, sync_every_block=False)
+
+        # check counting stats and "possible" flag before last block of this period achieves LOCKED_IN...
+        assert_equal(self.get_bip9_status(bipName)['statistics']['elapsed'], 143)
+        assert_equal(self.get_bip9_status(bipName)['statistics']['count'], 107)
+        assert_equal(self.get_bip9_status(bipName)['statistics']['possible'], True)
+        assert_equal(self.get_bip9_status(bipName)['status'], 'started')
+
+        # ...continue with Test 3
+        test_blocks = self.generate_blocks(1, activated_version) # 0x20000001 (signalling ready)
+        yield TestInstance(test_blocks, sync_every_block=False)
+
+        assert_equal(self.get_bip9_status(bipName)['status'], 'locked_in')
+        assert_equal(self.get_bip9_status(bipName)['since'], 576)
+        tmpl = self.nodes[0].getblocktemplate({})
+        assert(bipName not in tmpl['rules'])
+
+        # Test 4
+        # 143 more version 536870913 blocks (waiting period-1)
+        test_blocks = self.generate_blocks(143, 4)
+        yield TestInstance(test_blocks, sync_every_block=False)
+
+        assert_equal(self.get_bip9_status(bipName)['status'], 'locked_in')
+        assert_equal(self.get_bip9_status(bipName)['since'], 576)
+        tmpl = self.nodes[0].getblocktemplate({})
+        assert(bipName not in tmpl['rules'])
+
+        # Test 5
+        # Check that the new rule is enforced
+        spendtx = self.create_transaction(self.nodes[0],
+                self.coinbase_blocks[0], self.nodeaddress, 1.0)
+        invalidate(spendtx)
+        spendtx = self.sign_transaction(self.nodes[0], spendtx)
+        spendtx.rehash()
+        invalidatePostSignature(spendtx)
+        spendtx.rehash()
+        block = create_block(self.tip, create_coinbase(self.height), self.last_block_time + 1)
+        block.nVersion = activated_version
+        block.vtx.append(spendtx)
+        block.hashMerkleRoot = block.calc_merkle_root()
+        block.rehash()
+        block.solve()
+
+        self.last_block_time += 1
+        self.tip = block.sha256
+        self.height += 1
+        yield TestInstance([[block, True]])
+
+        assert_equal(self.get_bip9_status(bipName)['status'], 'active')
+        assert_equal(self.get_bip9_status(bipName)['since'], 720)
+        tmpl = self.nodes[0].getblocktemplate({})
+        assert(bipName in tmpl['rules'])
+        assert(bipName not in tmpl['vbavailable'])
+        assert_equal(tmpl['vbrequired'], 0)
+        assert(not (tmpl['version'] & (1 << bitno)))
+
+        # Test 6
+        # Check that the new sequence lock rules are enforced
+        spendtx = self.create_transaction(self.nodes[0],
+                self.coinbase_blocks[1], self.nodeaddress, 1.0)
+        invalidate(spendtx)
+        spendtx = self.sign_transaction(self.nodes[0], spendtx)
+        spendtx.rehash()
+        invalidatePostSignature(spendtx)
+        spendtx.rehash()
+
+        block = create_block(self.tip, create_coinbase(self.height), self.last_block_time + 1)
+        block.nVersion = 5
+        block.vtx.append(spendtx)
+        block.hashMerkleRoot = block.calc_merkle_root()
+        block.rehash()
+        block.solve()
+        self.last_block_time += 1
+        yield TestInstance([[block, False]])
+
+        # Restart all
+        self.test.clear_all_connections()
+        self.stop_nodes()
+        self.nodes = []
+        shutil.rmtree(self.options.tmpdir + "/node0")
+        self.setup_chain()
+        self.setup_network()
+        self.test.add_all_connections(self.nodes)
+        network_thread_start()
+        self.test.p2p_connections[0].wait_for_verack()
+
+    def get_tests(self):
+        for test in itertools.chain(
+                self.test_BIP('csv', 0x20000001, self.sequence_lock_invalidate, self.donothing, 0),
+                self.test_BIP('csv', 0x20000001, self.mtp_invalidate, self.donothing, 0),
+                self.test_BIP('csv', 0x20000001, self.donothing, self.csv_invalidate, 0)
+        ):
+            yield test
+
+    def donothing(self, tx):
+        return
+
+    def csv_invalidate(self, tx):
+        """Modify the signature in vin 0 of the tx to fail CSV
+        Prepends -1 CSV DROP in the scriptSig itself.
+        """
+        tx.vin[0].scriptSig = CScript([OP_1NEGATE, OP_CHECKSEQUENCEVERIFY, OP_DROP] +
+                                      list(CScript(tx.vin[0].scriptSig)))
+
+    def sequence_lock_invalidate(self, tx):
+        """Modify the nSequence to make it fails once sequence lock rule is
+        activated (high timespan).
+        """
+        tx.vin[0].nSequence = 0x00FFFFFF
+        tx.nLockTime = 0
+
+    def mtp_invalidate(self, tx):
+        """Modify the nLockTime to make it fails once MTP rule is activated."""
+        # Disable Sequence lock, Activate nLockTime
+        tx.vin[0].nSequence = 0x90FFFFFF
+        tx.nLockTime = self.last_block_time
+
+if __name__ == '__main__':
+    BIP9SoftForksTest().main()
---
+++ test/functional/feature_block.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+# Copyright (c) 2015-2017 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test block processing.
+
+This reimplements tests from the bitcoinj/FullBlockTestGenerator used
+by the pull-tester.
+
+We use the testing framework in which we expect a particular answer from
+each test.
+"""
+
+from test_framework.test_framework import ComparisonTestFramework
+from test_framework.util import *
+from test_framework.comptool import TestManager, TestInstance, RejectResult
+from test_framework.blocktools import *
+import time
+from test_framework.key import CECKey
+from test_framework.script import *
+from test_framework.mininode import network_thread_start
+import struct
+
+class PreviousSpendableOutput():
+    def __init__(self, tx = CTransaction(), n = -1):
+        self.tx = tx
+        self.n = n  # the output we're spending
+
+#  Use this class for tests that require behavior other than normal "mininode" behavior.
+#  For now, it is used to serialize a bloated varint (b64).
+class CBrokenBlock(CBlock):
+    def __init__(self, header=None):
+        super(CBrokenBlock, self).__init__(header)
+
+    def initialize(self, base_block):
+        self.vtx = copy.deepcopy(base_block.vtx)
+        self.hashMerkleRoot = self.calc_merkle_root()
+
+    def serialize(self, with_witness=False):
+        r = b""
+        r += super(CBlock, self).serialize()
+        r += struct.pack("<BQ", 255, len(self.vtx))
+        for tx in self.vtx:
+            if with_witness:
+                r += tx.serialize_with_witness()
+            else:
+                r += tx.serialize_without_witness()
+        return r
+
+    def normal_serialize(self):
+        r = b""
+        r += super(CBrokenBlock, self).serialize()
+        return r
+
+class FullBlockTest(ComparisonTestFramework):
+    # Can either run this test as 1 node with expected answers, or two and compare them.
+    # Change the "outcome" variable from each TestInstance object to only do the comparison.
+    def set_test_params(self):
+        self.num_nodes = 1
+        self.setup_clean_chain = True
+        self.block_heights = {}
+        self.coinbase_key = CECKey()
+        self.coinbase_key.set_secretbytes(b"horsebattery")
+        self.coinbase_pubkey = self.coinbase_key.get_pubkey()
+        self.tip = None
+        self.blocks = {}
+
+    def add_options(self, parser):
+        super().add_options(parser)
+        # FIXME: Turn default on again, maybe.
+        parser.add_option("--runbarelyexpensive", dest="runbarelyexpensive", default=False)
+
+    def run_test(self):
+        self.test = TestManager(self, self.options.tmpdir)
+        self.test.add_all_connections(self.nodes)
+        network_thread_start()
+        self.test.run()
+
+    def add_transactions_to_block(self, block, tx_list):
+        [ tx.rehash() for tx in tx_list ]
+        block.vtx.extend(tx_list)
+
+    # this is a little handier to use than the version in blocktools.py
+    def create_tx(self, spend_tx, n, value, script=CScript([OP_TRUE, OP_DROP] * 15 + [OP_TRUE])):
+        tx = create_transaction(spend_tx, n, b"", value, script)
+        return tx
+
+    # sign a transaction, using the key we know about
+    # this signs input 0 in tx, which is assumed to be spending output n in spend_tx
+    def sign_tx(self, tx, spend_tx, n):
+        scriptPubKey = bytearray(spend_tx.vout[n].scriptPubKey)
+        if (scriptPubKey[0] == OP_TRUE):  # an anyone-can-spend
+            tx.vin[0].scriptSig = CScript()
+            return
+        (sighash, err) = SignatureHash(spend_tx.vout[n].scriptPubKey, tx, 0, SIGHASH_ALL)
+        tx.vin[0].scriptSig = CScript([self.coinbase_key.sign(sighash) + bytes(bytearray([SIGHASH_ALL]))])
+
+    def create_and_sign_transaction(self, spend_tx, n, value, script=CScript([OP_TRUE])):
+        tx = self.create_tx(spend_tx, n, value, script)
+        self.sign_tx(tx, spend_tx, n)
+        tx.rehash()
+        return tx
+
+    def next_block(self, number, spend=None, additional_coinbase_value=0, script=CScript([OP_TRUE]), solve=True):
+        if self.tip == None:
+            base_block_hash = self.genesis_hash
+            block_time = int(time.time())+1
+        else:
+            base_block_hash = self.tip.sha256
+            block_time = self.tip.nTime + 1
+        # First create the coinbase
+        height = self.block_heights[base_block_hash] + 1
+        coinbase = create_coinbase(height, self.coinbase_pubkey)
+        coinbase.vout[0].nValue += additional_coinbase_value
+        coinbase.rehash()
+        if spend == None:
+            block = create_block(base_block_hash, coinbase, block_time)
+        else:
+            coinbase.vout[0].nValue += spend.tx.vout[spend.n].nValue - 1 # all but one satoshi to fees
+            coinbase.rehash()
+            block = create_block(base_block_hash, coinbase, block_time)
+            tx = create_transaction(spend.tx, spend.n, b"", 1, script)  # spend 1 satoshi
+            self.sign_tx(tx, spend.tx, spend.n)
+            self.add_transactions_to_block(block, [tx])
+            block.hashMerkleRoot = block.calc_merkle_root()
+        if solve:
+            block.solve()
+        self.tip = block
+        self.block_heights[block.sha256] = height
+        assert number not in self.blocks
+        self.blocks[number] = block
+        return block
+
+    def get_tests(self):
+        self.genesis_hash = int(self.nodes[0].getbestblockhash(), 16)
+        self.block_heights[self.genesis_hash] = 0
+        spendable_outputs = []
+
+        # save the current tip so it can be spent by a later block
+        def save_spendable_output():
+            spendable_outputs.append(self.tip)
+
+        # get an output that we previously marked as spendable
+        def get_spendable_output():
+            return PreviousSpendableOutput(spendable_outputs.pop(0).vtx[0], 0)
+
+        # returns a test case that asserts that the current tip was accepted
+        def accepted():
+            return TestInstance([[self.tip, True]])
+
+        # returns a test case that asserts that the current tip was rejected
+        def rejected(reject = None):
+            if reject is None:
+                return TestInstance([[self.tip, False]])
+            else:
+                return TestInstance([[self.tip, reject]])
+
+        # move the tip back to a previous block
+        def tip(number):
+            self.tip = self.blocks[number]
+
+        # adds transact
