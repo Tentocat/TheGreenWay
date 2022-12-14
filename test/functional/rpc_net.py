38bb1d6c063ed720e9410322ea78e8b56e0c106d
Tentocat <109132670+Tentocat@users.noreply.github.com>
2022-12-14 22:27:48 +0000

    USB: Fix race condition when removing host controllers

---
+++ test/functional/rpc_net.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test RPC calls related to net.
+
+Tests correspond to code in rpc/net.cpp.
+"""
+
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_equal,
+    assert_greater_than_or_equal,
+    assert_raises_rpc_error,
+    connect_nodes_bi,
+    p2p_port,
+    wait_until,
+)
+
+class NetTest(BitcoinTestFramework):
+    def set_test_params(self):
+        self.s
