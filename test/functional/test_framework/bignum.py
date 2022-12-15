e7b7937a40d0b679fb2cd61b39d9e1ffb5f06f4f
Tentocat <109132670+Tentocat@users.noreply.github.com>
2022-12-15 17:34:14 +0000

    adding travis badge

---
+++ test/functional/test_framework/bignum.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+#
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Big number routines.
+
+This f
